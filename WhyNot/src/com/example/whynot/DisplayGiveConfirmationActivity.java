eb3565aa0d51948a5f13119b18ef223ecd9d4158
Korbin <kshuffleton@gmail.com>
2014-02-08 16:46:25 -0500

    Restored functionality, cycles between creating challenges and prompting to see if user wants to creat an additional challenge

--- WhyNot/src/com/example/whynot/DisplayGiveConfirmationActivity.java
+++ WhyNot/src/com/example/whynot/DisplayGiveConfirmationActivity.java
@@ -1,7 +1,5 @@
 package com.example.whynot;
 
-import java.util.ArrayList;
-
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
@@ -11,9 +9,6 @@ import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
-
-import com.firebase.client.Firebase;
 
 public class DisplayGiveConfirmationActivity extends Activity{
 	
@@ -23,6 +18,48 @@ public class DisplayGiveConfirmationActivity extends Activity{
 	 * 		* onCreateOptionsMenu
 	 * 		* onOptionsItemSelected
 	 */
+	
+	@SuppressLint("NewApi")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_display_give_confirmation);
+		// Show the Up button in the action bar.
+		setupActionBar();
+	}
+	
+	/**
+	 * Set up the {@link android.app.ActionBar}.
+	 */
+	private void setupActionBar() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.display_give_confirmation, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 	
 	// submit users challenge to Firebase
 	// THIS NEEDS TO BE CHANGED
--- WhyNot/src/com/example/whynot/MainActivity.java
+++ WhyNot/src/com/example/whynot/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.whynot;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Message;
 import android.view.Menu;
 import android.view.View;
 
